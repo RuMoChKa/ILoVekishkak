beb0f867afc3a1fcf991034df0e333ffbc94224b
RuMoChKa <163867272+RuMoChKa@users.noreply.github.com>
2024-03-18 16:55:57 +0300

    Кишленочка
    ШААШЛЫЫЫК

---
+++ main.js
@@ -0,0 +1,55 @@
+ let firstName = "Роман";
+ let lastName = "Селиванов";
+ let fullName = firstName + " " + lastName;
+ console.log(fullName);
+
+ let a = 1000;
+ let b = 7;
+ let c = 7;
+
+ console.log("a + b =", a + b);
+ console.log("a - b =", a - b);
+ console.log("a * b =", a * b);
+ console.log("a / b =", a / b);
+ console.log("a % b =", a % b);
+
+ let isTrue = true;
+ let isFalse = false;
+ console.log("isTrue && isFalse =", isTrue && isFalse);
+
+console.log("isTrue || isFalse =", isTrue || isFalse);
+ console.log("!isTrue =", !isTrue);
+
+ let counter = 10;
+ console.log("counter before increment:", counter);
+ counter++;
+ console.log("counter after increment:", counter);
+ counter--;
+ console.log("counter after decrement:", counter);
+
+ let greeting = "Я";
+ let name = "Рюмочка";
+ let message = greeting + ", " + name + "!";
+ console.log(message);
+
+ let prefix = "Тест";
+ let middle = "на";
+ let suffix = "человека";
+ let sentence = prefix + " " + middle + " " + suffix + ".";
+ let stn = `${prefix} ${middle} ${suffix}`
+ console.log(sentence);
+ console.log(stn);
+
+ let num1 = 15;
+ let num2 = 7;
+ let result = "Выход: " + (num1 + num2);
+ console.log(result);
+
+ let text = "Роооомоооочкаааа"; 
+ let upperCaseText = text.toUpperCase();
+let lowerCaseText = text.toLowerCase();
+console.log("Original Text:", text);
+console.log("Uppercase Text:", upperCaseText);
+console.log("Lowercase Text:", lowerCaseText);
+
+
